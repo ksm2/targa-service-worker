18ff0bc004d0fa3a59e3ccac5bfa364bda889337
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2018-09-04 14:38:17 +0200

    Improve Fetch API typings

--- typings/fetch.d.ts
+++ typings/fetch.d.ts
@@ -1,4 +1,44 @@
-interface Request {
+interface BlobPropertyBag {
+  type?: string;
+}
+
+interface Blob {
+  readonly size: number
+  readonly type: string
+
+  slice(start?: number, end?: number, contentType?: string): Blob
+}
+
+declare var Blob: {
+  prototype: Blob
+  new(blobParts?: BlobPart[], options?: BlobPropertyBag): Blob
+}
+
+interface Body {
+  readonly body: ReadableStream<Uint8Array>
+  readonly bodyUsed: boolean
+
+  arrayBuffer(): Promise<ArrayBuffer>
+  blob(): Promise<Blob>
+  json(): Promise<any>
+  text(): Promise<string>
+}
+
+interface Headers {
+  append(name: string, value: string): void;
+  delete(name: string): void;
+  get(name: string): string | null;
+  has(name: string): boolean;
+  set(name: string, value: string): void;
+  forEach(callbackfn: (value: string, key: string, parent: Headers) => void, thisArg?: any): void;
+}
+
+declare var Headers: {
+  prototype: Headers;
+  new(init?: HeadersInit): Headers;
+};
+
+interface Request extends Body {
   readonly url: string
   readonly method: string
 }
@@ -7,16 +47,24 @@ declare const Request: {
   new(url: string): Request
 }
 
-interface Response {
-  body: ReadableStream<Uint8Array>
+interface Response extends Body {
 }
 
-interface ResponseOptions {
-  headers: { [headerName: string]: string }
+interface ResponseInit {
+  headers?: HeadersInit;
+  status?: number;
+  statusText?: string;
 }
 
 declare const Response: {
-  new(rs: ReadableStream<Uint8Array>, options: ResponseOptions): Response
+  prototype: Response;
+  new(body?: BodyInit | null, init?: ResponseInit): Response;
+  error(): Response;
+  redirect(url: string, status?: number): Response;
 }
 
-declare function fetch(r: Request | string): Promise<Response>;
+declare function fetch(r: Request | string): Promise<Response>
+type BlobPart = BufferSource | Blob | string
+type BodyInit = Blob | BufferSource | URLSearchParams | ReadableStream<Uint8Array> | string
+type BufferSource = ArrayBufferView | ArrayBuffer
+type HeadersInit = Headers | string[][] | Record<string, string>;
